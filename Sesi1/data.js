24d204e973ff1278cf768defc0ed17df29b7f9c6
Leviathanzs <okywidhiansyah@gmail.com>
2022-09-28 11:04:34 +0700

    Break 59 minutes sesi 2

--- Sesi1/data.js
+++ Sesi1/data.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const nama = "Oky Satria Widhiansyah";
+let umur = 20;
+
+function generateBiodata() {
+    console.log(`Nama saya adalah ${nama} dan usia saya ${umur} tahun`);
+}
+
+console.log(nama);
+console.log(umur);
+generateBiodata();
