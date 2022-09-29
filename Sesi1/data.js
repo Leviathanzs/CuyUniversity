de72db092d36447fb41ff01cdc9b464e93c177ad
Leviathanzs <okywidhiansyah@gmail.com>
2022-09-29 13:04:29 +0700

    Completed sesi 2 JavaScript

--- Sesi1/data.js
+++ Sesi1/data.js
@@ -1,8 +1,25 @@
 const nama = "Oky Satria Widhiansyah";
 let umur = 20;
 
+let biodata = document.getElementById('biodata');
+
 function generateBiodata() {
-    console.log(`Nama saya adalah ${nama} dan usia saya ${umur} tahun`);
+    let generasi;
+
+    if (umur >= 10 && umur < 18) {
+       generasi = "Generasi remaja";
+    } else if (umur >= 18 && umur< 30) {
+        generasi = "Generasi dewasa";
+    } else if (umur >= 30) {
+        generasi = "Generasi tua";
+    } else if (umur >= 2 && umur < 10) {
+        generasi = "Generasi anak anak";
+    } else {
+        generasi = "Generasi bayi";
+    }
+
+    return biodata.innerHTML = generasi;
+
 }
 
 console.log(nama);
